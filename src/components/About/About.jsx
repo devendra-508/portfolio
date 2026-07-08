68598988345f4c7fad6ab7e0a63eab9a5850788d
Devendra rana <devendrarana370@gmail.com>
2026-07-08 10:26:53 +0530

    Fix profile image import path in About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/pro.png.JPG';
+import profileImage from '../../assets/pro.png';
 
 const About = () => {
   return (
