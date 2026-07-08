384af624ace783798427d5454a7f233b9e3cc735
Devendra rana <devendrarana370@gmail.com>
2026-07-08 11:12:51 +0530

    change pic

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/pro7.jpg';
+import profileImage from '../../assets/pro7.png.png';
 
 const About = () => {
   return (
