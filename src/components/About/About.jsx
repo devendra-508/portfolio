3daee76afb14c5b4730650721e34868ecf31f3b7
Devendra rana <devendrarana370@gmail.com>
2026-09-09 12:58:39 +0530

    Update About component link to new Google Drive URL

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
          
           <a
-            href="https://drive.google.com/file/d/1zSbDrO3oGj96oUATw_92ST8PLaGbdEcX/view?usp=drive_link"
+            href="https://drive.google.com/file/d/19zrl1rqpaPTZcNzXhVMOy4JF-50K5hoM/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
